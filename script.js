f71cf7cb0ca3c84b18e077aa9ca932a67bafc02e
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2022-04-09 02:23:40 +0530

    Final 1

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-'use strict'
+"use strict";
 
 /*console.log(document.querySelector('.message').textContent)
 //setting the text content
@@ -9,92 +9,129 @@ document.querySelector('.guess').value = 69
 console.log(document.querySelector('.guess').value)*/
 
 //State variables
-let secretNum = Math.trunc(Math.random() * 20) + 1
+let secretNum = Math.trunc(Math.random() * 20) + 1;
 
-let gleft = 7
+let gleft = 7;
 
-let highScore = 0
+let highScore = 0;
 
-let guess = 0
+let guess = 0;
 
-let score = 0
+let score = 0;
+
+//Modal and overlay objects
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
 
 //function to calculate score from the number of guesses left
-const calcScore = n => n * 10
+const calcScore = (n) => n * 10;
 
 //function to display suitable message
-const displayMessage = message =>
-  document.querySelector('.message').textContent = message
+const displayMessage = (message) =>
+  (document.querySelector(".message").textContent = message);
 
 //To handle the click event on `check`
 const handleClickCheck = function () {
-  guess = Number(document.querySelector('.guess').value)
-  console.log(guess, typeof guess)
+  guess = Number(document.querySelector(".guess").value);
+  console.log(guess, typeof guess);
 
   if (gleft > 1) {
     //No input
     if (!guess) {
-      displayMessage('⛔ No Number Entered.')
+      displayMessage("⛔ No Number Entered.");
 
       //Correct number
     } else if (guess === secretNum) {
-      displayMessage('Correct Number!!!')
-      document.querySelector('body').style.backgroundColor = '#3DC22C'
-      document.querySelector('.number').style.width = '30rem'
-      document.querySelector('.number').textContent = secretNum
-      score = calcScore(gleft)
-      document.querySelector('.score').textContent = score
-      highScore = Math.max(highScore, score)
-      document.querySelector('.highscore').textContent = highScore
-      document.getElementById('guessField').disabled = true
+      displayMessage("Correct Number!!!");
+      document.querySelector("body").style.backgroundColor = "#3DC22C";
+      document.querySelector(".number").style.width = "30rem";
+      document.querySelector(".number").textContent = secretNum;
+      score = calcScore(gleft);
+      document.querySelector(".score").textContent = score;
+      highScore = Math.max(highScore, score);
+      document.querySelector(".highscore").textContent = highScore;
+      document.getElementById("guessField").disabled = true;
 
       //Number not equal
     } else if (guess !== secretNum) {
-      if (guess < secretNum) displayMessage('📉 Too low! Enter again.') //Number too low
-      else displayMessage('📈 Too high! Enter again.')//Number too high
-      gleft--
-      score = calcScore(gleft)
-      document.querySelector('.guess-left').textContent = gleft
-      document.querySelector('.score').textContent = score
+      if (guess < secretNum)
+        displayMessage("📉 Too low! Enter again."); //Number too low
+      else displayMessage("📈 Too high! Enter again."); //Number too high
+      gleft--;
+      score = calcScore(gleft);
+      document.querySelector(".guess-left").textContent = gleft;
+      document.querySelector(".score").textContent = score;
 
       //Game Over
     }
   } else {
-    gleft = 0
-    score = calcScore(gleft)
-    document.querySelector('.score').textContent = score
-    document.querySelector('.guess-left').textContent = gleft
-    displayMessage('👊🔴 GAME OVER!!! 🔴👊')
-    document.querySelector('body').style.backgroundColor = '#ff3131'
-    document.getElementById('guessField').disabled = true
+    gleft = 0;
+    score = calcScore(gleft);
+    document.querySelector(".score").textContent = score;
+    document.querySelector(".guess-left").textContent = gleft;
+    displayMessage("👊🔴 GAME OVER!!! 🔴👊");
+    document.querySelector("body").style.backgroundColor = "#ff3131";
+    document.getElementById("guessField").disabled = true;
   }
-}
+};
 
 //Handling click event on `Again`
 const handleReset = function () {
-  secretNum = Math.trunc(Math.random() * 20) + 1
-  score = 0
-  gleft = 7
-  document.querySelector('.number').textContent = '?'
-  document.getElementById('guessField').disabled = false
-  document.querySelector('.score').textContent = score
-  document.querySelector('.guess-left').textContent = gleft
-  document.querySelector('body').style.backgroundColor = '#222'
-  document.querySelector('.number').style.width = '15rem'
-  displayMessage('Start guessing...')
+  secretNum = Math.trunc(Math.random() * 20) + 1;
+  score = 0;
+  gleft = 7;
+  document.querySelector(".number").textContent = "?";
+  document.getElementById("guessField").disabled = false;
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".guess-left").textContent = gleft;
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  displayMessage("Start guessing...");
   //document.getElementById('guessField').reset()
-  document.querySelector('.guess').value = ''
-}
+  document.querySelector(".guess").value = "";
+};
+
+//Handling click event on `Instructions`
+const handleInstructions = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+//Event handler for close instructions
+const handleCloseInstuctions = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
 
 //Event Listener for click event on `Check`
-document.querySelector('.btn.check').addEventListener('click', handleClickCheck)
+document
+  .querySelector(".btn.check")
+  .addEventListener("click", handleClickCheck);
 
 //Event Listener for global `keyup` event
-document.addEventListener('keyup', function (e) {
-  if (e.key === 'Enter') {
-    handleClickCheck()
+document.addEventListener("keyup", function (e) {
+  if (e.key === "Enter") {
+    handleClickCheck();
   }
-})
+});
 
 //Event Listener for click event on `Again`
-document.querySelector('.btn.again').addEventListener('click', handleReset)
+document.querySelector(".btn.again").addEventListener("click", handleReset);
+
+//Event Listener for click on 'Inctructions'
+document
+  .querySelector(".btn.instruc")
+  .addEventListener("click", handleInstructions);
+
+//Close Instructions
+//Click X
+document
+  .querySelector(".close-modal")
+  .addEventListener("click", handleCloseInstuctions);
+//Click overlay
+overlay.addEventListener("click", handleCloseInstuctions);
+//Keydown ESC
+document.addEventListener("keydown", function (e) {
+  if (e.key == "Escape" && !modal.classList.contains("hidden"))
+    handleCloseInstuctions();
+});
